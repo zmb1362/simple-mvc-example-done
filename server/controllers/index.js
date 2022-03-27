d9a6b88cfb50044282780d3f6dd57021b4db55a0
Zachary Blair <zachary.m.blair@gmail.com>
2022-03-27 19:39:02 -0400

    Removed comments for circleci and eslint

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -12,14 +12,10 @@ const defaultData = {
   bedsOwned: 0,
 };
 
-// object for us to keep track of the last Cat we made and dynamically update it sometimes
 let lastAdded = new Cat(defaultData);
 
 // Function to handle rendering the index page.
 const hostIndex = (req, res) => {
-  /* res.render will render the given view from the views folder. In this case, index.
-     We pass it a number of variables to populate the page.
-  */
   res.render('index', {
     currentName: lastAdded.name,
     title: 'Home',
@@ -30,46 +26,12 @@ const hostIndex = (req, res) => {
 // Function for rendering the page1 template
 // Page1 has a loop that iterates over an array of cats
 const hostPage1 = async (req, res) => {
-  /* Remember that our database is an entirely separate server from our node
-     code. That means all interactions with it are async, and just because our
-     server is up doesn't mean our database is. Therefore, any time we
-     interact with it, we need to account for scenarios where it is not working.
-     That is why the code below is wrapped in a try/catch statement.
-  */
   try {
-    /* We want to find all the cats in the Cat database. To do this, we need
-       to make a "query" or a search. Queries in Mongoose are "thenable" which
-       means they work like promises. Since they work like promises, we can also
-       use await/async with them.
-
-       The result of any query will either throw an error, or return zero, one, or
-       multiple "documents". Documents are what our database stores. It is often
-       abbreviated to "doc" or "docs" (one or multiple).
-
-       .find() is a function in all Mongoose models (like our Cat model). It takes
-       in an object as a parameter that defines the search. In this case, we want
-       to find every cat, so we give it an empty object because that will not filter
-       out any cats.
-
-       .lean() is a modifier for the find query. Instead of returning entire mongoose
-       documents, .lean() will only return the JS Objects being stored. Try printing
-       out docs with and without .lean() to see the difference.
-
-       .exec() executes the chain of operations. It is not strictly necessary and
-       can be removed. However, mongoose gives better error messages if we use it.
-    */
     const docs = await Cat.find({}).lean().exec();
 
     // Once we get back the docs array, we can send it to page1.
     return res.render('page1', { cats: docs });
   } catch (err) {
-    /* If our database returns an error, or is unresponsive, we will print that error to
-       our console for us to see. We will also send back an error message to the client.
-
-       We don't want to send back the err from mongoose, as that would be unsafe. You
-       do not want people to see actual error messages from your server or database, or else
-       they can exploit them to attack your server.
-    */
     console.log(err);
     return res.status(500).json({ error: 'failed to find cats' });
   }
@@ -110,12 +72,6 @@ const setName = async (req, res) => {
     // If they are missing data, send back an error.
     return res.status(400).json({ error: 'firstname, lastname and beds are all required' });
   }
-
-  /* If they did send all the data, we want to create a cat and add it to our database.
-     We begin by creating a cat that matches the format of our Cat schema. In this case,
-     we define a name and bedsOwned. We don't need to define the createdDate, because the
-     default Date.now function will populate that value for us later.
-  */
   const catData = {
     name: `${req.body.firstname} ${req.body.lastname}`,
     bedsOwned: req.body.beds,
@@ -129,25 +85,8 @@ const setName = async (req, res) => {
   */
   const newCat = new Cat(catData);
 
-  /* We have now setup a cat in the right format. We now want to store it in the database.
-     Again, because the database and node server are separate things entirely we have no
-     way of being sure the database will work or respond. Because of that, we wrap our code
-     in a try/catch.
-  */
   try {
-    /* newCat is a version of our catData that is database-friendly. If you print it, you will
-       see it has extra information attached to it other than name and bedsOwned. One thing it
-       now has is a .save() function. This function will intelligently add or update the cat in
-       the database. Since we have never saved this cat before, .save() will create a new cat in
-       the database. All calls to the database are async, including .save() so we will await the
-       databases response. If something goes wrong, we will end up in our catch() statement.
-    */
     await newCat.save();
-
-    /* After our await has resolved, and if no errors have occured during the await, we will end
-       up here. We will update our lastAdded cat to the one we just added. We will then send that
-       cat's data to the client.
-    */
     lastAdded = newCat;
     return res.json({
       name: lastAdded.name,
@@ -173,12 +112,6 @@ const setDog = async (req, res) => {
     age: req.body.age,
   };
 
-  /* Once we have our cat object set up. We want to turn it into something the database
-     can understand. To do this, we create a new instance of a Cat using the Cat model
-     exported from the Models folder.
-
-     Note that this does NOT store the cat in the database. That is the next step.
-  */
   const newDog = new Dog(dogData);
   try {
     await newDog.save();
@@ -199,32 +132,10 @@ const setDog = async (req, res) => {
 
 // Function to handle searching a cat by name.
 const searchName = async (req, res) => {
-  /* When the user makes a POST request, bodyParser populates req.body with the parameters
-     as we saw in setName() above. In the case of searchName, the user is making a GET request.
-     GET requests do not have a body, but they can have query parameters. bodyParser will also
-     handle these, and store them in req.query instead.
-
-     If the user does not give us a name to search by, throw an error.
-  */
   if (!req.query.name) {
     return res.status(400).json({ error: 'Name is required to perform a search' });
   }
-
-  /* If they do give us a name to search, we will as the database for a cat with that name.
-     Remember that since we are interacting with the database, we want to wrap our code in a
-     try/catch in case the database throws an error or doesn't respond.
-  */
   try {
-    /* Just like Cat.find() in hostPage1() above, Mongoose models also have a .findOne()
-       that will find a single document in the database that matches the search parameters.
-       This function is faster, as it will stop searching after it finds one document that
-       matches the parameters. The downside is you cannot get multiple responses with it.
-
-       One of three things will occur when trying to findOne in the database.
-        1) An error will be thrown, which will stop execution of the try block and move to the catch block.
-        2) Everything works, but the name was not found in the database returning an empty doc object.
-        3) Everything works, and an object matching the search is found.
-    */
     const doc = await Cat.findOne({ name: req.query.name }).exec();
 
     // If we do not find something that matches our search, doc will be empty.
@@ -273,28 +184,13 @@ const updateLast = (req, res) => {
   // First we will update the number of bedsOwned.
   lastAdded.bedsOwned++;
 
-  /* Remember that lastAdded is a Mongoose document (made on line 14 if no new
-     ones were made after the server started, or line 116 if there was). Mongo
-     documents have an _id, which is a globally unique identifier that distinguishes
-     them from other documents. Our mongoose document also has this _id. When we
-     call .save() on a document, Mongoose and Mongo will use the _id to determine if
-     we are creating a new database entry (if the _id doesn't already exist), or
-     if we are updating an existing entry (if the _id is already in the database).
-
-     Since lastAdded is likely already in the database, .save() will update it rather
-     than make a new cat.
-
-     We can use async/await for this, or just use standard promise .then().catch() syntax.
-  */
   const savePromise = lastAdded.save();
 
-  // If we successfully save/update them in the database, send back the cat's info.
   savePromise.then(() => res.json({
     name: lastAdded.name,
     beds: lastAdded.bedsOwned,
   }));
 
-  // If something goes wrong saving to the database, log the error and send a message to the client.
   savePromise.catch((err) => {
     console.log(err);
     return res.status(500).json({ error: 'Something went wrong' });
@@ -313,7 +209,6 @@ const updateAge = (req, res) => {
     age: lastAdded.age,
   }));
 
-  // If something goes wrong saving to the database, log the error and send a message to the client.
   savePromise.catch((err) => {
     console.log(err);
     return res.status(500).json({ error: 'Something went wrong' });
--- server/models/Cat.js
+++ server/models/Cat.js
@@ -1,43 +1,6 @@
-// require mongoose, a popular MongoDB library for nodejs
-// Mongoose connects in the app.js file. Once mongoose is connected,
-// it stays connected across all of the files in this project
-// As long as you import this after you have connected,
-// then mongoose will give you the active DB connection which is what we want
 const mongoose = require('mongoose');
 
-// variable to hold our Model
-// A Model is our data structure to handle data. This can be an object, JSON, XML or anything else.
-// A mongoDB model is a Mongo database structure with the API attached
-// That is, a model has built-in functions for its data structure like find, findOne, etc.
-// Usually you will retrieve data from the database through the Model object
 let CatModel = {};
-
-/* While Mongo is a schema-less database, meaning we can just store arbitrary objects in it, Mongoose
-   does implement a schema system. If you wanted to just store arbitrary objects you could just use
-   the mongoDB NodeJS driver: https://www.npmjs.com/package/mongodb
-
-   Information about the differences between MongoDB Driver and Mongoose found here:
-   https://www.mongodb.com/developer/article/mongoose-versus-nodejs-driver/
-
-   The schema enforces a semi-rigid format on our data so that it is more of a "known quantity". This
-   puts us somewhere between the very rigid SQL format and the very loose Mongo format.
-
-   To create the schema we give it an object. Each top level key (like name, bedsOwned, and createdDate)
-   defines a variable our objects can have. We then use individual objects to define the features and parameters
-   of each one of those variables.
-
-   For our name, we define a type of String. This means name is a string. Since mongo stores javascript
-   objects, the type usually does not matter. However, this is something added by Mongoose to clarify what each
-   variable should contain. We also give it the required option, which means every object in the cat database MUST
-   have a name. We set the trim option to true, which will remove trailing and leading whitespace from the string.
-   Finally we set the unique option to true. This means that there cannot be two cats with the same name.
-
-   For our bedsOwned, we say it is a number. We set the minimum value to 0. We say it is required.
-
-   For our createdDate, we want to store the time that the cat was first added to the database. Rather than
-   figure that out in our own code when we create each cat, we can instead set it's default to the Date.now function.
-   When a new cat is made, it will execute the default function and return the current date.
-*/
 const CatSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -58,11 +21,7 @@ const CatSchema = new mongoose.Schema({
   },
 
 });
-// Create the cat model based on the schema. You provide it with a custom discriminator
-// (the name of the object type. Can be anything)
-// and the schema to make a model from.
-// Look at the model variable definition above for more details.
+
 CatModel = mongoose.model('Cat', CatSchema);
 
-// We only want to export the cat model, so we can overwrite the entire exports object.
 module.exports = CatModel;
